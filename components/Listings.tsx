00d4e12d36622ed9758887dcafd2ba016e543499
Kim Jisena <kimjisena@gmail.com>
2024-01-19 16:37:53 +0300

    misc: minor changes

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,18 +1,35 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, ListRenderItem } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
+import { defaultStyles } from "@/constants/Styles";
 
 interface ListingsProps {
   listings: any[];
   category: string;
 }
-const Listings = ({ listings, category }: ListingsProps) => {
+const Listings = ({ listings: items, category }: ListingsProps) => {
+  const [loading, setLoading] = React.useState(false);
+  const listRef = React.useRef<FlatList>(null);
   React.useEffect(() => {
-    console.log("RELOAD LISTINGS FOR_", listings.length);
+    console.log("RELOAD LISTINGS FOR_", items.length);
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
   }, [category]);
 
+  const renderRow: ListRenderItem<any> = ({ item }) => {
+    return <Link href={`/listing/${item.id}`}>Go there</Link>;
+  };
+
   return (
-    <View>
-      <Text>Listings</Text>
+    <View style={defaultStyles.container}>
+      <FlatList
+        renderItem={renderRow}
+        ref={listRef}
+        data={loading ? [] : items}
+      />
     </View>
   );
 };
