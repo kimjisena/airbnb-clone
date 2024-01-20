1a77b685bbca2d781348f2255bfa6d688bc888ab
Kim Jisena <kimjisena@gmail.com>
2024-01-20 17:26:08 +0300

    misc: minor changes

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,10 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
+import listingsData from "@/assets/data/airbnb-listings.json";
 
 const Page = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
-  console.log("🚀 ~ Page ~ id:", id);
   return (
     <View>
       <Text>Page</Text>
