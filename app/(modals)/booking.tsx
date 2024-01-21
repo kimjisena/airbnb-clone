e9983784d3c55223be22b29c9308b157acc3e69c
Kim Jisena <kimjisena@gmail.com>
2024-01-21 13:53:54 +0300

    misc: minor changes

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,7 +1,11 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import React from "react";
 import { BlurView } from "expo-blur";
-import Animated, { SlideInDown } from "react-native-reanimated";
+import Animated, {
+  FadeIn,
+  SlideInDown,
+  FadeOut,
+} from "react-native-reanimated";
 import { defaultStyles } from "@/constants/Styles";
 import { useRouter } from "expo-router";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -24,10 +28,80 @@ const Page = () => {
       {/** where card */}
       <View style={styles.card}>
         {openCard !== 0 && (
-          <AnimatedTouchableOpacity onPress={() => setOpenCard(0)}>
+          <AnimatedTouchableOpacity
+            onPress={() => setOpenCard(0)}
+            style={styles.cardPreview}
+            entering={FadeIn.duration(200)}
+            exiting={FadeOut.duration(200)}
+          >
             <Text style={styles.previewText}>Where</Text>
             <Text style={styles.previewDate}>I'm flexible</Text>
           </AnimatedTouchableOpacity>
+        )}
+        {openCard === 0 && (
+          <>
+            <Animated.Text entering={FadeIn} style={styles.cardHeader}>
+              Where to?
+            </Animated.Text>
+            <Animated.View style={styles.cardBody}>
+              <View style={styles.searchSection}>
+                <Ionicons
+                  name="ios-search"
+                  size={20}
+                  style={styles.searchIcon}
+                />
+                <TextInput
+                  style={styles.searchInputField}
+                  placeholder="Search destination"
+                  placeholderTextColor={Colors.grey}
+                />
+              </View>
+            </Animated.View>
+          </>
+        )}
+      </View>
+
+      {/** when card */}
+      <View style={styles.card}>
+        {openCard !== 1 && (
+          <AnimatedTouchableOpacity
+            onPress={() => setOpenCard(1)}
+            style={styles.cardPreview}
+            entering={FadeIn.duration(200)}
+            exiting={FadeOut.duration(200)}
+          >
+            <Text style={styles.previewText}>When</Text>
+            <Text style={styles.previewDate}>Any week</Text>
+          </AnimatedTouchableOpacity>
+        )}
+        {openCard === 1 && (
+          <Animated.View>
+            <Animated.Text entering={FadeIn} style={styles.cardHeader}>
+              When's your trip?
+            </Animated.Text>
+          </Animated.View>
+        )}
+      </View>
+
+      {/** who card */}
+      <View style={styles.card}>
+        {openCard !== 2 && (
+          <AnimatedTouchableOpacity
+            onPress={() => setOpenCard(2)}
+            style={styles.cardPreview}
+            entering={FadeIn.duration(200)}
+            exiting={FadeOut.duration(200)}
+          >
+            <Text style={styles.previewText}>Who</Text>
+            <Text style={styles.previewDate}>Add guests</Text>
+          </AnimatedTouchableOpacity>
+        )}
+        {openCard === 2 && (
+          <Animated.View>
+            <Animated.Text entering={FadeIn} style={styles.cardHeader}>
+              Who's coming?
+            </Animated.Text>
+          </Animated.View>
         )}
       </View>
       {/** footer */}
@@ -103,6 +177,39 @@ const styles = StyleSheet.create({
     fontFamily: "mon-sb",
     fontSize: 14,
     colors: Colors.dark,
+  },
+  cardPreview: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  cardHeader: {
+    fontFamily: "mon-b",
+    fontSize: 24,
+    padding: 20,
+  },
+  cardBody: {
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+  },
+  searchSection: {
+    height: 50,
+    flexDirection: "row",
+    borderColor: "#ababab",
+    borderRadius: 8,
+    borderWidth: 1,
+    backgroundColor: "#fff",
+    alignContent: "center",
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  searchInputField: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  searchIcon: {
+    padding: 10,
   },
 });
 
