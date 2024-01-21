ee3a49cbe5e03b027fae6deb0ea67c7e04b878b3
Kim Jisena <kimjisena@gmail.com>
2024-01-21 12:20:25 +0300

    misc: minor changes

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -6,13 +6,31 @@ import { defaultStyles } from "@/constants/Styles";
 import { useRouter } from "expo-router";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
+const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity);
 const Page = () => {
   const router = useRouter();
-  const onClearAll = () => {};
+  const [openCard, setOpenCard] = React.useState(0);
+  const [selectedPlace, setSelectedPlace] = React.useState(0);
+
+  const onClearAll = () => {
+    setSelectedPlace(0);
+    setOpenCard(0);
+  };
   return (
     <BlurView intensity={70} style={styles.container} tint="light">
-      <Text>Booking</Text>
+      {/** where card */}
+      <View style={styles.card}>
+        {openCard !== 0 && (
+          <AnimatedTouchableOpacity onPress={() => setOpenCard(0)}>
+            <Text style={styles.previewText}>Where</Text>
+            <Text style={styles.previewDate}>I'm flexible</Text>
+          </AnimatedTouchableOpacity>
+        )}
+      </View>
+      {/** footer */}
       <Animated.View
         style={defaultStyles.footer}
         entering={SlideInDown.delay(200)}
@@ -49,7 +67,7 @@ const Page = () => {
               color="#fff"
               style={defaultStyles.btnIcon}
             />
-            <Text style={defaultStyles.btnText}>Clear all</Text>
+            <Text style={defaultStyles.btnText}>Search</Text>
           </TouchableOpacity>
         </View>
       </Animated.View>
@@ -61,6 +79,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 100,
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 14,
+    margin: 10,
+    elevation: 4,
+    shadowColor: "#000",
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    gap: 20,
+  },
+  previewText: {
+    fontFamily: "mon-sb",
+    fontSize: 14,
+    colors: Colors.grey,
+  },
+  previewDate: {
+    fontFamily: "mon-sb",
+    fontSize: 14,
+    colors: Colors.dark,
   },
 });
 
